73d69bd48bc0072d277008863566a566bf3552a5
Sergey Yamshchikov <sergey.yamshchikov@scout24.com>
2021-01-08 17:58:50 +0100

    initial commit

---
+++ pyautoproxy/main.py
@@ -0,0 +1,122 @@
+import argparse
+import re
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse
+
+class RequestFilter:
+
+    def validate(self, handler: BaseHTTPRequestHandler) -> bool:
+        return True;
+
+class DenyAllFilter(RequestFilter):
+
+    def validate(self, handler: BaseHTTPRequestHandler) -> bool:
+        return False;        
+
+class IosSimulatorFilter(RequestFilter):
+
+    def validate(self, handler: BaseHTTPRequestHandler) -> bool:
+        try:
+            if re.search('^CFNetworkAgent.*CFNetwork.*Darwin.*\d$', handler.headers['User-Agent']):
+                return True
+            else:
+                return False    
+        except:
+            return False 
+
+class CompositeRequestFilter(RequestFilter):
+
+    filters = list[RequestFilter]
+
+    def __init__(self, filters: list[RequestFilter]) -> None:
+        super().__init__()
+        self.filters = filters;
+
+    def validate(self, handler: BaseHTTPRequestHandler) -> bool:
+        for filter in self.filters:
+            if filter.validate(handler=handler):
+                return True
+        return False        
+
+filter = RequestFilter();
+
+class AutoProxyServer(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/x-ns-proxy-autoconfig')
+        self.send_header('Cache-Control', 'no-cache')
+        self.send_header('Cache-Control', 'no-store')
+        self.end_headers()
+
+    def _autoproxy_string(self, host, port):
+        content = "function FindProxyForURL(url, host) {{ return \"PROXY {host}:{port}; DIRECT\"; }}".format(host=host, port=port)
+
+        return content.encode("utf8")
+
+    def do_GET(self):
+        try:
+            if filter.validate(handler=self):
+                self._set_headers()
+                query = urlparse(self.path).query
+                query_components = dict(qc.split("=") for qc in query.split("&"))
+
+                requested_port = query_components['port']
+                requested_host = query_components['host']
+
+                self.wfile.write(self._autoproxy_string(host=requested_host, port=requested_port))
+            else:
+                self.send_response(502)
+                self.wfile.write('Request doesn\'t satisfy any configured filters'.encode('utf8'))                    
+        except:
+            self.send_response(502)
+            self.wfile.write('Error while handling request'.encode('utf8'))
+
+
+    def do_HEAD(self):
+        self._set_headers()
+
+
+def run(server_class=HTTPServer, addr="localhost", port=8000):
+    httpd = HTTPServer((addr, port), AutoProxyServer)
+
+    print("Starting httpd server on {addr}:{port}".format(addr=addr, port=port))
+
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Configurable autoproxy')
+    parser.add_argument(
+        '-l',
+        '--listen',
+        default="localhost",
+        required=False,
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        '-p', 
+        '--port', 
+        type=int, 
+        default='8081', 
+        required=False, 
+        help='Specify the port on which the server listens')
+
+    parser.add_argument(
+        '-s',
+        '--simulator',
+        action='store_true',
+        default=False,
+        required=False,
+        help='Send proxy configuration only to requests from iOS simulator'
+    )    
+
+    args = parser.parse_args()
+
+    if args.simulator:
+        filter = CompositeRequestFilter(filters= [
+            DenyAllFilter(),
+            IosSimulatorFilter()
+        ])
+
+    run(addr=args.listen, port=args.port)
---
+++ setup.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+import setuptools
+from setuptools.command.install import install
+
+
+def version() -> str: return "0.0.46"
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != version():
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, version()
+            )
+            sys.exit(info)
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyautoproxy",
+    version=version(),
+    author="Sergey Yamshchikov",
+    author_email="yamsergey@gmail.com",
+    description="Light weight auto proxy server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yamsergey/rikki",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    },
+    entry_points={
+        'console_scripts': [
+            "pyautoproxy = pyautoproxy:__main__"
+        ]
+    }
+)
